cfdc90d13fc5670ce751473d2b36dc327331ad1f
Nawer <nam.ngh2211@gmail.com>
2021-11-04 15:53:51 +0700

    Update

---
+++ src/JavaLession1/TinhSoCanNangCoThe.java
@@ -0,0 +1,28 @@
+package JavaLession1;
+
+import java.util.Scanner;
+
+public class TinhSoCanNangCoThe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+
+        System.out.println("Your weight in kilogram : ");
+        weight = scanner.nextDouble();
+
+        System.out.println("Your height in m : ");
+        height = scanner.nextDouble();
+
+        bmi = weight / (height * 2);
+        System.out.println("Your bmi is: " + bmi);
+
+        if (bmi < 18)
+            System.out.println("Underweight");
+        else if (bmi < 25.0)
+            System.out.println("Normal");
+        else if (bmi < 30.0)
+            System.out.println("Overweight");
+        else
+            System.out.println("Obese");
+    }
+}
