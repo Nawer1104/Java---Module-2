556d597883c0b2414d8f3b44198df32332af32a9
Nawer <nam.ngh2211@gmail.com>
2021-11-04 15:56:40 +0700

    Update

---
+++ src/JavaLession1/HienThiLoiChao.java
@@ -0,0 +1,13 @@
+package JavaLession1;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
