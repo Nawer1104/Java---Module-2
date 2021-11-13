61eb18c4b990a47bcd3a464f9a7fd6d34a901846
Nawer <nam.ngh2211@gmail.com>
2021-11-13 11:45:12 +0700

    Update

---
+++ src/JavaLesson7/InterfaceComparator/CircleComparator.java
@@ -0,0 +1,14 @@
+package JavaLesson7.InterfaceComparator;
+
+import JavaLesson6.CacHeDoiTuongHinhHoc.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
---
+++ src/JavaLesson7/InterfaceComparator/CircleComparatorTest.java
@@ -0,0 +1,29 @@
+package JavaLesson7.InterfaceComparator;
+
+import JavaLesson6.CacHeDoiTuongHinhHoc.Circle;
+import JavaLesson7.InterfaceComparable.ComparableCircle;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle("indigo", false, 3.5);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
