264509ddd0d50c399dd165c8ab7bbc06cacce32c
Nawer <nam.ngh2211@gmail.com>
2021-11-15 12:57:10 +0700

    Update

---
+++ src/JavaLesson9/SimpleCalculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package JavaLesson9.SimpleCalculator;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
