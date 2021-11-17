856da429747b6bd4aa7350116746982aa4663bd2
Nawer <nam.ngh2211@gmail.com>
2021-11-17 13:32:36 +0700

    Update test

---
+++ src/JavaLesson10/FakeList/MyListTest.java
@@ -0,0 +1,21 @@
+package JavaLesson10.FakeList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>();
+
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(3, 4);
+        myList.add(5);
+        myList.print();
+
+        System.out.println();
+        System.out.println(myList.get(0));
+        System.out.println(myList.indexOf(5));
+        myList.clear();
+        myList.print();
+
+    }
+}
